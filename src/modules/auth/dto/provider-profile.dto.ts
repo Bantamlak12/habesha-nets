4493d596451949ba57f06c2264f9448c4c856f0e
Bantamlak <neoluka315@gmail.com>
2024-08-27 17:14:16 +0300

    new dtos

---
+++ src/modules/auth/dto/provider-profile.dto.ts
@@ -0,0 +1,33 @@
+import { IsOptional, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import { LocationDto } from './location.dto';
+
+export class ProviderProfileDto {
+  @IsString()
+  @ApiProperty({ example: 'John' })
+  firstName: string;
+
+  @IsString()
+  @ApiProperty({ example: 'Doe' })
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ example: 'john12@gmail.com' })
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ example: '+25424545475' })
+  phoneNumber: string;
+
+  @IsString()
+  @ApiProperty({ example: 'Upload image image' })
+  profilePicture: string;
+
+  @ValidateNested()
+  @Type(() => LocationDto)
+  @ApiProperty({ type: LocationDto })
+  location: LocationDto;
+}
