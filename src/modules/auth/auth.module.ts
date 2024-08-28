dc5df41fc60428dacca90c426f23e1d972ad9d5c
Bantamlak <neoluka315@gmail.com>
2024-08-28 22:56:09 +0300

    auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,9 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { MailerConfigModule } from 'src/shared/mailer/mailer.module';
 import { SmsModule } from 'src/shared/sms/sms.module';
+import { UploadModule } from 'src/shared/upload/upload.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), MailerConfigModule, SmsModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    MailerConfigModule,
+    SmsModule,
+    UploadModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
