39dd543876355d5771177f1838c3b10864c4df02
Bantamlak <neoluka315@gmail.com>
2024-08-29 17:24:11 +0300

    repository entities imported

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,14 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../users/entities/user.entity';
 import { MailerConfigModule } from 'src/shared/mailer/mailer.module';
 import { SmsModule } from 'src/shared/sms/sms.module';
 import { UploadModule } from 'src/shared/upload/upload.module';
+import { Employer } from '../users/entities/employer.entity';
+import { Freelancer } from '../users/entities/freelancer.entity';
+import { ServiceProvider } from '../users/entities/serviceProvider';
+import { PropertyOwner } from '../users/entities/propertyOwner.entity';
+import { PropertyRenter } from '../users/entities/propertyRenter.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([
+      Employer,
+      Freelancer,
+      ServiceProvider,
+      PropertyOwner,
+      PropertyRenter,
+    ]),
     MailerConfigModule,
     SmsModule,
     UploadModule,
