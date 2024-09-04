a025b5b7eed92c761fde4546221c2901a050f45d
Bantamlak <neoluka315@gmail.com>
2024-09-04 20:36:59 +0300

    a new users entity that is used as a parent table for all tables

---
+++ src/modules/users/entities/users.entity.ts
@@ -0,0 +1,62 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  TableInheritance,
+} from 'typeorm';
+
+@Entity('users')
+@TableInheritance({ column: { type: 'varchar' } })
+export abstract class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  userType: string;
+
+  @Column({ default: false })
+  isVerified: boolean;
+
+  @Column({ nullable: true })
+  verificationCode: string;
+
+  @Column({ nullable: true })
+  verificationCodeExpires: Date;
+
+  @Column({ nullable: true })
+  firstName: string;
+
+  @Column({ nullable: true })
+  lastName: string;
+
+  @Column({ unique: true, nullable: true })
+  email: string;
+
+  @Column({ unique: true, nullable: true })
+  phoneNumber: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  profilePicture: string;
+
+  @Column({ type: 'jsonb', nullable: true })
+  location: {
+    city: string;
+    state: string;
+    country: string;
+  };
+
+  @Column({ type: 'enum', enum: ['Phone', 'Email', 'SMS'], default: 'Email' })
+  preferredContactMethod: 'Phone' | 'Email' | 'SMS';
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ nullable: true })
+  subscriptionStatus: 'subscribed' | 'unsubscribed';
+
+  @Column({ nullable: true })
+  subscriptionPlan: 'monthly' | 'sixMonth' | 'Yearly' | null;
+}
