71fe662f31879aa065251f2e5c819deabf174e03
Bantamlak <neoluka315@gmail.com>
2024-09-04 20:45:19 +0300

    a new signin dto

---
+++ src/modules/auth/dto/signin-user.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail({}, { message: 'Invalid email format' })
+  @ApiProperty({
+    example: 'example@gmail.com',
+  })
+  email: string;
+
+  @IsString()
+  @ApiProperty({
+    example: 'password',
+  })
+  password: string;
+}
