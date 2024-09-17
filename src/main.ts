36fab233aeb36714cecbbe7811c902c83a5d7611
Bantamlak <neoluka315@gmail.com>
2024-09-17 12:27:14 +0300

    default cors options used instead

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost'],
-    methods: 'GET, POST, PATCH, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  });
+  app.enableCors();
 
   app.use(cookieParser());
 
