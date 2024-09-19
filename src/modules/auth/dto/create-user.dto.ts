b74586ac13ba9c4397c9cb8cfd32abc4a81d8069
Bantamlak <neoluka315@gmail.com>
2024-09-19 11:25:03 +0300

    create user dto updated to accept either phoneNumber or email

--- src/modules/auth/dto/create-user.dto.ts
+++ src/modules/auth/dto/create-user.dto.ts
@@ -9,7 +9,6 @@ import {
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmailOrPhone } from 'src/common/is-phone-or-email.validator';
 
-@IsEmailOrPhone()
 export class CreateUserDto {
   @IsString()
   @IsIn([
@@ -23,18 +22,16 @@ export class CreateUserDto {
   userType: string;
 
   @IsOptional()
-  @IsEmail({}, { message: 'Invalid email format' })
-  @ApiProperty({
-    example: 'example@gmail.com',
-  })
-  email: string;
+  email?: string;
 
   @IsOptional()
-  @IsPhoneNumber(null, { message: 'Invalid phone number format' })
-  //   @ApiProperty({
-  //     example: '+12192643829',
-  //   })
   phoneNumber?: string;
+
+  @IsEmailOrPhone()
+  @ApiProperty({
+    example: '+2514454523457',
+  })
+  emailOrPhoneNumber: string;
 
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
