e1b1d90c31e07dd3f27ae01226f48c49c1d1a4f2
Bantamlak <neoluka315@gmail.com>
2024-09-19 17:31:55 +0300

    paypal integration on progress

--- src/modules/paypal/paypal.controller.ts
+++ src/modules/paypal/paypal.controller.ts
@@ -1,23 +1,71 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { Request as ExpressRequest } from 'express';
 import { PaypalService } from './paypal.service';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('paypal')
 export class PaypalController {
   constructor(private readonly paypalService: PaypalService) {}
 
-  @Post('create-order')
-  async createOrder(@Body('plan') plan: string) {
-    const order = await this.paypalService.createOrder(plan);
-
-    return {
-      id: order.id,
-      links: order.links.filter((link) => link.rel === 'approve'),
-    };
+  @Post('get-token')
+  async getToken() {
+    const tokenResponse = await this.paypalService.getAccessToken();
+    return tokenResponse;
   }
 
-  @Post('capture/:orderId')
-  async captureOrder(@Param('orderId') orderId: string) {
-    const capture = await this.paypalService.captureOrder(orderId);
-    return capture;
+  //create product
+  @Post('create-product')
+  async createProduct() {
+    const result = await this.paypalService.createProduct();
+    return result;
+  }
+
+  //Create Plan
+  @Post('create-plan')
+  async createPlan(@Body() body: any) {
+    return await this.paypalService.createBillingPlan(
+      body.PaypalRequestId,
+      body.name,
+      body.description,
+      body.amount,
+      body.interval_count,
+      body.interval_unit,
+    );
+  }
+
+  //Create Subscription
+  @Post('create-subscription/:planId')
+  @UseGuards(JwtAuthGuard)
+  async createSubscription(
+    @Param('planId') planId: string,
+    @Request() req: ExpressRequest,
+  ) {
+    const parameterId = parseInt(planId, 10);
+    const userId = req.user?.['sub'];
+    if (!parameterId) {
+      throw new HttpException('Invalid parameter ID', HttpStatus.BAD_REQUEST);
+    }
+    try {
+      const subscription = await this.paypalService.createSubscription(
+        parameterId,
+        userId,
+      );
+      return subscription;
+    } catch (error) {
+      throw new HttpException(
+        `Failed to create PayPal subscription: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
--- src/modules/paypal/paypal.service.ts
+++ src/modules/paypal/paypal.service.ts
@@ -1,18 +1,38 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
 import { AxiosResponse } from 'axios';
-import { lastValueFrom } from 'rxjs';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OAuth2Token } from './entities/token.entity';
+import { Repository } from 'typeorm';
+import { AuthService } from '../auth/auth.service';
+import { Product } from './entities/product.entity';
+import { BillingPlan } from './entities/billing.entity';
 
 @Injectable()
 export class PaypalService {
   private PAYPAL_API: string;
+  private readonly logger = new Logger(PaypalService.name);
 
   constructor(
     private readonly httpService: HttpService,
     private readonly config: ConfigService,
+    private readonly authService: AuthService,
+    @InjectRepository(OAuth2Token)
+    private readonly tokenRepo: Repository<OAuth2Token>,
+    @InjectRepository(Product)
+    private readonly productRepo: Repository<Product>,
+    @InjectRepository(BillingPlan)
+    private readonly billingRepo: Repository<BillingPlan>,
   ) {
     this.PAYPAL_API = this.config.get<string>('PAYPAL_API');
+  }
+
+  async getToken() {
+    const encyptedToken = (await this.tokenRepo.findOneBy({})).token;
+    const accessToken = await this.authService.decyptPaypalToken(encyptedToken);
+    return accessToken;
   }
 
   async getAccessToken(): Promise<string> {
@@ -39,6 +59,10 @@ export class PaypalService {
           requestConfig,
         ),
       );
+      await this.saveOrUpdateToken(
+        response.data.access_token,
+        response.data.expires_in,
+      );
       return response.data.access_token;
     } catch (err) {
       throw new HttpException(
@@ -48,74 +72,297 @@ export class PaypalService {
     }
   }
 
-  async createOrder(plan: string): Promise<any> {
-    const accessToken = await this.getAccessToken();
+  // Save token
+  private async saveOrUpdateToken(
+    token: string,
+    expiresIn: number,
+  ): Promise<void> {
+    const existingToken = await this.tokenRepo.findOneBy({});
+    const encryptedToken = await this.authService.encryptPaypalToken(token);
 
-    let price = 0;
-    if (plan === 'monthly') price = 14.99;
-    else if (plan === 'six-month') price = 64.99;
-    else if (plan === 'yearly') price = 99.99;
+    if (existingToken) {
+      existingToken.token = encryptedToken;
+      existingToken.expiresIn = expiresIn;
+      existingToken.updatedAt = new Date();
+      await this.tokenRepo.save(existingToken);
+    } else {
+      const newToken = this.tokenRepo.create({
+        token: encryptedToken,
+        expiresIn,
+        updatedAt: new Date(),
+      });
+      await this.tokenRepo.save(newToken);
+    }
+  }
+
+  // Create product
+  async createProduct(): Promise<any> {
+    const paypalApiUrlProduct = `${this.PAYPAL_API}/v1/catalogs/products`;
 
     const requestBody = {
-      intent: 'CAPTURE',
-      purchase_units: [
-        {
-          amount: {
-            currency_code: 'USD',
-            value: price.toFixed(2),
-          },
-        },
-      ],
+      name: 'Subscription Payment',
+      description: 'HabeshaNets Website Subscription Product Service',
+      type: 'SERVICE',
+      category: 'SOFTWARE',
     };
 
+    const accessToken = await this.getToken();
     const requestConfig = {
       headers: {
         Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
+        Prefer: 'return=representation',
       },
     };
 
     try {
-      const response: AxiosResponse = await lastValueFrom(
-        this.httpService.post(
-          `${this.PAYPAL_API}/v2/checkout/orders`,
-          requestBody,
-          requestConfig,
-        ),
+      const response = await firstValueFrom(
+        this.httpService.post(paypalApiUrlProduct, requestBody, requestConfig),
       );
+      // Extract data from response
+      const productData = response.data;
+
+      // Save product data to the database
+      const product = this.productRepo.create({
+        id: productData.id,
+        name: productData.name,
+        description: productData.description,
+        type: productData.type,
+        category: productData.category,
+        create_time: new Date(productData.create_time),
+        update_time: new Date(productData.create_time),
+      });
+      await this.productRepo.save(product);
       return response.data;
-    } catch (err) {
-      throw new HttpException(
-        'Failed to create paypal order',
-        err.response?.status || 500,
+    } catch (error) {
+      console.error('PayPal API Error:', error.response?.data || error.message);
+      throw new Error(
+        `Failed to create PayPal product: ${error.response?.data?.message || error.message}`,
       );
     }
   }
 
-  async captureOrder(orderId: string): Promise<any> {
-    const accessToken = await this.getAccessToken();
+  //Create Billing Plan For subscription
+  async createBillingPlan(
+    paypalRequestId: string,
+    name: string,
+    description: string,
+    amount: string,
+    interval_count: number,
+    interval_unit: string,
+  ): Promise<any> {
+    const paypalUrlBillingPlan = `${this.PAYPAL_API}/v1/billing/plans`;
 
+    const accessToken = await this.getToken();
     const requestConfig = {
       headers: {
         Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
+        'PayPal-Request-Id': paypalRequestId,
+        Prefer: 'return=representation',
+      },
+    };
+
+    const productId = (await this.productRepo.findOneBy({})).id;
+    const data = {
+      product_id: productId,
+      name,
+      description,
+      status: 'ACTIVE',
+      billing_cycles: [
+        {
+          tenure_type: 'REGULAR',
+          sequence: 1,
+          frequency: {
+            interval_unit: interval_unit,
+            interval_count: interval_count,
+          },
+          total_cycles: 0, // 0 indicates unlimited cycles
+          pricing_scheme: {
+            fixed_price: {
+              value: amount,
+              currency_code: 'USD',
+            },
+          },
+        },
+      ],
+      payment_preferences: {
+        auto_bill_outstanding: true,
+        setup_fee: {
+          value: '1',
+          currency_code: 'USD',
+        },
+        setup_fee_failure_action: 'CONTINUE',
+        payment_failure_threshold: 3,
+      },
+      taxes: {
+        percentage: '10',
+        inclusive: false,
       },
     };
 
     try {
-      const response: AxiosResponse = await lastValueFrom(
-        this.httpService.post(
-          `${this.PAYPAL_API}/v2/checkout/orders/${orderId}/capture`,
-          {},
-          requestConfig,
-        ),
+      const response = await lastValueFrom(
+        this.httpService.post(paypalUrlBillingPlan, data, requestConfig),
       );
+      this.logger.log('Billing plan created successfully');
+      const billingPlanData = response.data;
+      this.logger.log('Billing plan data:', billingPlanData);
+
+      const billing = new BillingPlan();
+      billing.id = billingPlanData.id;
+      billing.product_id = billingPlanData.product_id;
+      billing.name = billingPlanData.name;
+      billing.description = billingPlanData.description;
+      billing.status = billingPlanData.status;
+      const firstCycle = billingPlanData.billing_cycles[0];
+      if (firstCycle) {
+        billing.tenure_type = firstCycle.tenure_type; // Regular or Trial
+        billing.interval_count = firstCycle.frequency.interval_count;
+        billing.interval_unit = firstCycle.frequency.interval_unit;
+
+        if (
+          firstCycle.pricing_scheme &&
+          firstCycle.pricing_scheme.fixed_price
+        ) {
+          billing.currency_code =
+            firstCycle.pricing_scheme.fixed_price.currency_code;
+          billing.value = firstCycle.pricing_scheme.fixed_price.value;
+        } else {
+          this.logger.error(
+            'Fixed price information is missing in pricing scheme',
+          );
+        }
+      } else {
+        this.logger.error('Billing cycle information is missing');
+      }
+
+      billing.links = billingPlanData.links;
+      billing.created_time = new Date(); // Default to current time if missing
+      billing.update_time = new Date(); // Default to current time if missing
+
+      await this.billingRepo.save(billing);
+
       return response.data;
-    } catch (err) {
-      throw new HttpException(
-        'Failed to capture paypal order',
-        err.response?.status || 500,
+    } catch (error) {
+      this.logger.error(
+        'Error creating PayPal billing plan',
+        error.response ? error.response.data : error,
       );
+      throw error;
     }
+  }
+
+  async createSubscription(parameterId: number, userId: string) {
+    // let subscriptionId: string | null = null;
+    let subscriptionPlan: string | null = null;
+
+    const subscriptionId = await this.billingRepo.find();
+    console.log(userId);
+    console.log(subscriptionId);
+    //   if (parameterId === 1) {
+    //     subscriptionId = process.env.Month_1_Subscription_ID;
+    //     subscriptionPlan = 'monthly';
+    //   } else if (parameterId === 2) {
+    //     subscriptionId = process.env.Month_6_Subscription_ID;
+    //     subscriptionPlan = 'sixMonth';
+    //   } else if (parameterId === 3) {
+    //     subscriptionId = process.env.YEAR_1_Subscription_ID;
+    //     subscriptionPlan = 'Yearly';
+    //   } else {
+    //     throw new BadRequestException('incorrect URL Request');
+    //   }
+
+    //   // Fetch the user data from the database
+    //   const user = await this.employerRepository.findOne({
+    //     where: { id: userId },
+    //   });
+    //   if (!user) {
+    //     throw new BadRequestException('User not found');
+    //   }
+
+    //   const requestBody = {
+    //     plan_id: subscriptionId,
+    //     quantity: '1',
+    //     subscriber: {
+    //       name: { given_name: user.firstName, surname: user.lastName },
+    //       email_address: user.email,
+    //       shipping_address: {
+    //         name: { full_name: `${user.firstName} ${user.lastName}` },
+    //         address: {
+    //           address_line_1: '2211 N First Street',
+    //           address_line_2: 'Building 17',
+    //           admin_area_2: 'San Jose',
+    //           admin_area_1: 'CA',
+    //           postal_code: '95131',
+    //           country_code: 'US',
+    //         },
+    //       },
+    //     },
+    //     application_context: {
+    //       brand_name: 'HabeshaNet',
+    //       locale: 'en-US',
+    //       shipping_preference: 'SET_PROVIDED_ADDRESS',
+    //       user_action: 'SUBSCRIBE_NOW',
+    //       payment_method: {
+    //         payer_selected: 'PAYPAL',
+    //         payee_preferred: 'IMMEDIATE_PAYMENT_REQUIRED',
+    //       },
+    //       return_url: 'https://www.godigitalethio.com/news/returnUrl',
+    //       cancel_url: 'https://www.google.com/cancelUrl',
+    //     },
+    //   };
+
+    //   const headers = {
+    //     Authorization: this.accessToken,
+    //     'Content-Type': 'application/json',
+    //     Accept: 'application/json',
+    //     'PayPal-Request-Id': `SUBSCRIPTION-${Date.now()}`,
+    //     Prefer: 'return=representation',
+    //   };
+
+    //   try {
+    //     const response = await firstValueFrom(
+    //       this.httpService.post(this.paypalApiUrl, requestBody, { headers }),
+    //     );
+    //     const approveLink = response.data.links.find(
+    //       (link) => link.rel === 'approve',
+    //     )?.href;
+    //     console.log(approveLink);
+    //     this.logger.log(response.data);
+
+    //     const subscriptionData = new Subscription();
+    //     const subscription = response.data;
+
+    //     subscriptionData.id = subscription.id;
+    //     subscriptionData.status = subscription.status;
+    //     subscriptionData.status_update_time = new Date(
+    //       subscription.status_update_time,
+    //     );
+    //     subscriptionData.plan_id = subscription.plan_id;
+    //     subscriptionData.user_Id = userId;
+    //     subscriptionData.start_time = new Date(subscription.start_time);
+    //     subscriptionData.subscriber_given_name =
+    //       subscription.subscriber.name.given_name;
+    //     subscriptionData.subscriber_surname =
+    //       subscription.subscriber.name.surname;
+    //     subscriptionData.subscriber_email_address =
+    //       subscription.subscriber.email_address;
+    //     subscriptionData.create_time = new Date(subscription.create_time);
+    //     //  subscriptionData.payer_id = subscription.subscriber.payer_id;
+    //     subscriptionData.subscriptio_links = subscription.links;
+    //     await this.subscriptionRepository.save(subscriptionData);
+    //     await this.employerRepository.update(
+    //       { id: userId },
+    //       { subscriptionPlan, SubscriptionUpdated: new Date() },
+    //     );
+    //     // return response.data;
+    //     return approveLink;
+    //   } catch (error) {
+    //     console.error('PayPal API Error:', error.respomse?.data || error.message);
+    //     throw new Error(
+    //       `Failed to create PayPal subscription: ${error.response?.data?.message || error.message}`,
+    //     );
+    //   }
   }
 }
