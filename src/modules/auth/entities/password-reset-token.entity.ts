2843428e32a48519499ebcc28e2984de7c97adda
Bantamlak <neoluka315@gmail.com>
2024-09-23 17:24:43 +0300

    entity added for the password reset token

---
+++ src/modules/auth/entities/password-reset-token.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from 'src/modules/users/entities/users.entity';
+
+@Entity()
+export class ResetTokens {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: true })
+  resetToken: string;
+
+  @Column({ type: 'timestamp', nullable: true })
+  resetTokenExpiry: Date;
+
+  @ManyToOne(() => User, (user) => user.resetTokens, {
+    onDelete: 'CASCADE',
+  })
+  user: User;
+}
