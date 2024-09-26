f33706c515859964f1977d97c7047a31303ac9ac
Bantamlak <neoluka315@gmail.com>
2024-09-26 10:07:14 +0300

    file renamed

--- src/shared/mailer/mailer.service.ts
+++ src/shared/mailer/mailer.service.ts
@@ -7,8 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Subscription } from 'src/modules/paypal/entities/subscription.entity';
 import { subscriptionconformationEmail } from 'src/shared/mailer/templates/subscription.template';
-import { accountVerificationEmail } from 'src/shared/mailer/templates/account-verification.template';
-import { subscriptioncancelEmail } from './templates/subscription.cancel.template';
+import { subscriptioncancelEmail } from './templates/subscription-cancel.template';
 
 @Injectable()
 export class CustomMailerService {
