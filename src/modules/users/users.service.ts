d8e41e9f2da8b38a0b3ca3afe2e5e4f5d4b89ca1
Bantamlak <neoluka315@gmail.com>
2024-10-07 15:01:40 +0300

    profile updates are enabled to users

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -209,13 +209,113 @@ export class UsersService {
     const updatedUser = await this.userRepo.update(userId, {
       firstName: capitalizeString(body.firstName),
       lastName: capitalizeString(body.lastName),
-      email: user.email ? user.email : body.email,
-      phoneNumber: user.phoneNumber ? user.phoneNumber : body.phoneNumber,
+      secondaryEmail: user.secondaryEmail,
+      secondaryPhoneNumber: user.secondaryPhoneNumber,
       bio: body.bio,
       profilePicture: profileURL,
       preferredContactMethod: body.preferredContactMethod,
       address: body.address,
       propertyType: body.propertyType,
+    });
+
+    return updatedUser;
+  }
+
+  async updatePropertyRenterProfile(
+    userId: string,
+    body: any,
+    profileImg: Express.Multer.File,
+  ) {
+    // Check if the user exists
+    const user = await this.userRepo.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    let profileURL: string | undefined;
+    if (profileImg && process.env.NODE_ENV === 'development') {
+      profileURL = await this.uploadService.uploadFile(profileImg, 'images');
+    } else if (profileImg && process.env.NODE_ENV === 'production') {
+      profileURL = await this.uploadService.uploadFileToS3(profileImg);
+    }
+
+    // Update the fields
+    const updatedUser = await this.userRepo.update(userId, {
+      firstName: capitalizeString(body.firstName),
+      lastName: capitalizeString(body.lastName),
+      secondaryEmail: user.secondaryEmail,
+      secondaryPhoneNumber: user.secondaryPhoneNumber,
+      bio: body.bio,
+      profilePicture: profileURL,
+      preferredContactMethod: body.preferredContactMethod,
+      address: body.address,
+      budgetRange: body.budgetRange,
+    });
+
+    return updatedUser;
+  }
+
+  async updateBabySitterFinderProfile(
+    userId: string,
+    body: any,
+    profileImg: Express.Multer.File,
+  ) {
+    // Check if the user exists
+    const user = await this.userRepo.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    let profileURL: string | undefined;
+    if (profileImg && process.env.NODE_ENV === 'development') {
+      profileURL = await this.uploadService.uploadFile(profileImg, 'images');
+    } else if (profileImg && process.env.NODE_ENV === 'production') {
+      profileURL = await this.uploadService.uploadFileToS3(profileImg);
+    }
+
+    // Update the fields
+    const updatedUser = await this.userRepo.update(userId, {
+      firstName: capitalizeString(body.firstName),
+      lastName: capitalizeString(body.lastName),
+      secondaryEmail: user.secondaryEmail,
+      secondaryPhoneNumber: user.secondaryPhoneNumber,
+      bio: body.bio,
+      profilePicture: profileURL,
+      preferredContactMethod: body.preferredContactMethod,
+      address: body.address,
+    });
+
+    return updatedUser;
+  }
+
+  async updateCareGiverFinderProfile(
+    userId: string,
+    body: any,
+    profileImg: Express.Multer.File,
+  ) {
+    // Check if the user exists
+    const user = await this.userRepo.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    let profileURL: string | undefined;
+    if (profileImg && process.env.NODE_ENV === 'development') {
+      profileURL = await this.uploadService.uploadFile(profileImg, 'images');
+    } else if (profileImg && process.env.NODE_ENV === 'production') {
+      profileURL = await this.uploadService.uploadFileToS3(profileImg);
+    }
+
+    // Update the fields
+    const updatedUser = await this.userRepo.update(userId, {
+      firstName: capitalizeString(body.firstName),
+      lastName: capitalizeString(body.lastName),
+      secondaryEmail: user.secondaryEmail,
+      secondaryPhoneNumber: user.secondaryPhoneNumber,
+      bio: body.bio,
+      profilePicture: profileURL,
+      preferredContactMethod: body.preferredContactMethod,
+      address: body.address,
     });
 
     return updatedUser;
